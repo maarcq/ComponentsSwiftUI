c15ac0ab65d96f74eeec86fdd7dca62e82a50260
maarcq <marcellerq27@gmail.com>
2025-03-02 21:01:28 -0300

    Initial Commit

---
+++ ComponentsSwiftUI/AnimatedMeshGradient.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedMeshGradient.swift
+//  ComponentsSwiftUI
+//
+//  Created by Marcelle Ribeiro Queiroz on 02/03/25.
+//
+
+import SwiftUI
+
+struct AnimatedMeshGradient: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AnimatedMeshGradient()
+}
---
+++ ComponentsSwiftUI/ComponentsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsSwiftUIApp.swift
+//  ComponentsSwiftUI
+//
+//  Created by Marcelle Ribeiro Queiroz on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
