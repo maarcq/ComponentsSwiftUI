1195fd97f23b84588dbc7383a2c012bf36451b71
maarcq <marcellerq27@gmail.com>
2025-03-02 21:17:13 -0300

    ok

--- ComponentsSwiftUI/AnimatedMeshGradient.swift
+++ ComponentsSwiftUI/AnimatedMeshGradient.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct AnimatedMeshGradient: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        MeshGradient(
+            width: 3,
+            height: 3,
+            points: [ [0, 0], [0.5, 0], [1, 0], [0, 0.5], [0.5, 0.5], [1, 0.5], [0, 1], [0.5, 1], [1, 1] ],
+            colors: [.blue, .purple, .indigo, .orange, .yellow, .red, .green, .teal, .cyan]
+        )
+        .ignoresSafeArea()
     }
 }
 
