fbbbde06142cefb90f7ac0b56005ed48e39fd718
maarcq <marcellerq27@gmail.com>
2025-03-10 18:32:59 -0300

    ok ok

---
+++ ComponentsSwiftUI/Desing/MeshGradient.swift
@@ -0,0 +1,18 @@
+//
+//  MeshGradient.swift
+//  ComponentsSwiftUI
+//
+//  Created by Marcelle Ribeiro Queiroz on 10/03/25.
+//
+
+import SwiftUI
+
+struct MeshGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeshGradient()
+}
