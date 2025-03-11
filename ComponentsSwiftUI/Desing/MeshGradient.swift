43ac1df0649ff0dbca56cacc9c9381cc1c2cf506
maarcq <marcellerq27@gmail.com>
2025-03-10 21:08:49 -0300

    MeshGradient

--- ComponentsSwiftUI/Desing/MeshGradient.swift
+++ ComponentsSwiftUI/Desing/MeshGradient.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct MeshGradientView: View {
     var body: some View {
-        Text("Hello, World!")
+        MeshGradient(
+            width: 3,
+            height: 3,
+            points: [
+                [0, 0], [0.5, 0], [1, 0],
+                [0.5, 1], [0.7, 0.5], [1, 0.7],
+                [0, 1], [0, 0.5], [0, 0.5]
+            ],
+            colors: [
+                .teal, .purple, .indigo,
+                .purple, .blue, .pink,
+                .purple, .red, .purple
+            ]
+        )
+        .ignoresSafeArea()
+        .shadow(color: .gray, radius: 25, y: 10)
     }
 }
 
